836bd98c65f1dcf52e0651b1f5c2d75c86f96c8e
Vincent <vincentckhuang@gmail.com>
2016-07-21 00:07:49 -0700

    added bootstrap css and created header

--- pet_app/app/controllers/users_controller.rb
+++ pet_app/app/controllers/users_controller.rb
@@ -1,4 +1,51 @@
 class UsersController < ApplicationController
   def main
   end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to user_path @user[:id]
+  	else
+  		render :new
+  	end
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def show
+  	@user = set_user
+  end
+
+  def edit
+  	@user = set_user
+  end
+
+  def update
+  	@user = set_user
+  	if @user.update_attributes(user_params)
+  		redirect_to user_path
+  	else
+  		render :edit
+  	end
+  end
+
+  def delete
+  	@user = set_user
+  	@user.destroy
+  	redirect_to 'users/main'
+  end
+
+  private
+
+  def set_user
+  	User.find(params[:id])
+  end
+
+  def user_params
+  	params.require(:user).permit(:username, :password, :f_name, :l_name, :city, :prof_pic)
+  end
+
 end
--- pet_app/config/routes.rb
+++ pet_app/config/routes.rb
@@ -5,7 +5,21 @@ Rails.application.routes.draw do
 
   get 'pets/main'
 
+
+  ####
+  #USERS#
+  ####
   get 'users/main'
+  #create profile
+  post 'users' => 'users#create' #post
+  get 'users/new' => 'users#new', as: :new_user #retrieve form
+  #display profile
+  get 'users/:id' => 'users#show', as: :user 
+  #edit profile
+  get 'users/:id/edit' => 'users#edit', as: :edit_user 
+  patch 'users/:id' => 'users#update' 
+  #delete profile
+  delete 'users/:id' => 'users#delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
